69306decf3ede424dda0a853bba9edf45d85680b
sehyun hwang <sehyunny6@gmail.com>
2019-08-25 18:19:29 +0900

    brute_force_3

--- 1_bye_bye_company.cpp
+++ 1_bye_bye_company.cpp
@@ -2,8 +2,38 @@
 
 using namespace std;
 
-int main() {
+typedef struct {
+	int t;
+	int p;
+}DayInfo;
 
+DayInfo dayInfo[16];
+int maxProfit = -1;
+
+void calcProfit(int today, int profit, int n) {
+	if (today==n+1) {
+		if (profit > maxProfit)
+			maxProfit = profit;
+		return;
+	}
+	if (today > n) return;
+	//오늘이 선택될 경우
+	calcProfit(today + dayInfo[today].t, profit + dayInfo[today].p, n);
+	//오늘이 선택되지 않을 경우
+	calcProfit(today + 1, profit, n);
+}
+
+int main() {
+	int day;
+
+	cin >> day;
+
+	for (int i = 1; i <= day; i++)
+		cin >> dayInfo[i].t >> dayInfo[i].p;
+
+	calcProfit(1, 0, day);
+	
+	cout << maxProfit;
 
 	return 0;
 }
